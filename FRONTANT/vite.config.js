e3f286697d70e727ddb17d0b461716065b2b0450
FIROZ KHAN <computerpc096@gmail.com>
2025-04-24 07:01:52 -0500

    send

--- FRONTANT/vite.config.js
+++ FRONTANT/vite.config.js
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import React from 'react'
-export default defineConfig({
+import { VitePWA } from 'vite-plugin-pwa'
+import {tailwindcss} from 'tailwindcss'
+export default {
   plugins: [
     tailwindcss(),
-  ],
-})
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'images.jpg', 'icon-512x512.png'],
+      manifest: {
+        name: 'AI Chatbot by Firoz',
+        short_name: 'Firoz AI',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0f172a',
+        theme_color: '#1d4ed8',
+        icons: [
+          {
+            src: '/images.jpg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/images.jpg',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ]
+}
