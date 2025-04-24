1c85780beb31e24d54b895056f1b857de3fa3231
FIROZ KHAN <computerpc096@gmail.com>
2025-04-24 07:23:09 -0500

    changes

--- FRONTANT/vite.config.js
+++ FRONTANT/vite.config.js
@@ -1,32 +1,7 @@
-import { VitePWA } from 'vite-plugin-pwa'
-import tailwindcss from 'tailwindcss'
-export default {
-  plugins: [
-    tailwindcss(),
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'images.jpg', 'icon-512x512.png'],
-      manifest: {
-        name: 'AI Chatbot by Firoz',
-        short_name: 'Firoz AI',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#0f172a',
-        theme_color: '#1d4ed8',
-        icons: [
-          {
-            src: '/images.jpg',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/images.jpg',
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
-      }
-    })
-  ]
-}
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
